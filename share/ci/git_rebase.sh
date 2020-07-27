cedfc9146d6332169665d1f63e2521542241a06c
René Widera <r.widera@hzdr.de>
2020-07-27 13:14:01 +0200

    add github test token

--- share/ci/git_rebase.sh
+++ share/ci/git_rebase.sh
@@ -12,8 +12,9 @@ cd $CI_PROJECT_DIR
 github_group_repo="ComputationalRadiationPhysics/gitlab-ci-testproject"
 
 pr_id=$(echo "$CI_BUILD_REF_NAME" | cut -d"/" -f1 | cut -d"-" -f2)
-#curl -X GET https://api.github.com/repos/${github_group_repo}/pulls/${pr_id}
-target_branch=$(curl -X GET https://api.github.com/repos/${github_group_repo}/pulls/${pr_id} 2>/dev/null| python3 -c 'import json,sys;obj=json.loads(sys.stdin.read());print(obj["base"]["ref"])')
+
+curl_data=$(curl -u psychocoderHPC:$GITHUB_TOKEN -X GET https://api.github.com/repos/${github_group_repo}/pulls/${pr_id} 2>/dev/null)
+target_branch=$(echo -e "$curl_data" | python3 -c 'import json,sys;obj=json.loads(sys.stdin.read());print(obj["base"]["ref"])')
 echo "target_branch=${target_branch}"
 
 mainline_exists=$(git remote -v | cut -f1 | grep mainline -q && echo 0 || echo 1)
